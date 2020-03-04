6464b2ea76c8253419fb1efac27ddf97a0299552
Kifixo <danielhuici@hotmail.com>
2020-03-04 16:24:30 +0100

    update host files

--- app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
+++ app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -2,11 +2,14 @@ package info.androidhive.loginandregistration.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://centralteruel.ddns.net/instantm/login.php";
+	public static String URL_LOGIN = "http://34.69.44.48/instantm/login.php";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://centralteruel.ddns.net/instantm/registrar.php";
+	public static String URL_REGISTER = "http://34.69.44.48/instantm/registrar.php";
 
 	// Server create group url
-	public static String URL_CREATE_GROUP = "http://centralteruel.ddns.net/instantm/crear_grupo.php";
+	public static String URL_CREATE_GROUP = "http://34.69.44.48/instantm/crear_grupo.php";
+
+	// Server create group url
+	public static String URL_GET_GROUPS = "http://34.69.44.48/instantm/crear_grupo.php";
 }
